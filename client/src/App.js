c6f458ba7b0451204d75322e77d0ffad0eba1acc
Praveen Kumar <pkspyder007@gmail.com>
2020-10-09 14:51:12 +0530

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button, Text } from 'evergreen-ui';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>Button</Button>
+      <Text>hello world</Text>
+    </div>
+  );
+}
+
+export default App;
