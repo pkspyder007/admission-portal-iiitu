99c7d0f45eb2d5c425b29a9f51e7bc8d2f0205b1
Kushagra Johari <skjkushagra1309@gmail.com>
2020-10-19 14:31:05 +0530

    styled downloads

--- client/src/components/Downloads.jsx
+++ client/src/components/Downloads.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
+import '../css/Downloads.scss';
+import '../css/Field.scss'
+import HeaderInfo from './HeaderInfo';
 
 const Downloads = () => {
     return (
-        <div>
-            down
+        <>
+      <div className="header__info">
+      <h1>Indian Institute Of Information Technology Una (H.P)</h1>
+      <h2>Transit Campus-II Chandpur, Haroli, Una-177220</h2>
+      <h3>Btech Admissions</h3>
+      <h4>Documents Download</h4>
+      </div>
+        <div className="downloads__container">
+        <ul>
+            <li><a href="#" target="_blank" download="PDF">Download</a></li>
+            <li><a href="#" target="_blank" download="PDF">Download</a></li>
+            <li><a href="#" target="_blank" download="PDF">Download</a></li>
+            <li><a href="#" target="_blank" download="PDF">Download</a></li>
+            <li><a href="#" target="_blank" download="PDF">Download</a></li>
+        </ul>
         </div>
+        </>
     )
 }
 
