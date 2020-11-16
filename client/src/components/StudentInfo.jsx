6c027207578f2384ba65453fe0449eabf88c4e80
Praveen Kumar <pkspyder007@gmail.com>
2020-11-16 20:15:07 +0530

    updated air cat rank

--- client/src/components/StudentInfo.jsx
+++ client/src/components/StudentInfo.jsx
@@ -91,7 +91,7 @@ const StudentInfo = () => {
             <Info title="JEE (MAIN) AIR-CRL" value={std.jeeMainAirCrl} />
           </Row>
           <Row justify="space-between" gutter={[48, 24]}>
-            <Info title="JEE (MAIN) AIR-CATEGORY" value={"22"} />
+            <Info title="JEE (MAIN) AIR-CATEGORY" value={jeeMainAirCat} />
             <Info title="Admitted Category" value={std.admittedCategory} />
           </Row>
           <Row justify="space-between" gutter={[48, 24]}>
